ea31355f756eece6e3d8273eb74c36447bc989d2
Hafsa Binte Jakir <hafsa002>
2022-08-20 17:24:20 +0600

    weather app updated

--- app.js
+++ app.js
@@ -6,16 +6,21 @@ const weather = document.querySelector("#weather")
     // q=${city}&appid=${API_KEY}&units=metric`
     // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
 const getWeather = async(city) => {
-    weather.innerHTML = `<h4> Loading... <h4>`
+    if(city == ''){
+        alert("enter the city name");
+    }else{
+    weather.innerHTML = `<h4> Loading..... <h4>`
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     const response = await fetch(url);
     const data = await response.json()
     return showWeather(data)
 }
+}
 
 const showWeather = (data) => {
+
     if (data.cod == "404") {
-        weather.innerHTML = `<h4> City Not Found <h4>`
+        weather.innerHTML = `<h4  style= "color:red"> City Not Found <h4>`
         return;
     }
     weather.innerHTML = `
